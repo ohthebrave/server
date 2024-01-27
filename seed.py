19e26d0125f6078148a5e20053681189b7d407da
George <georgejoj28@gmail.com>
2024-01-28 01:57:21 +0300

    add image url

--- seed.py
+++ seed.py
@@ -3,117 +3,176 @@ from faker import Faker
 from app import app
 from models import User, Product, Order, Cart, Address, Transaction, Category
 
-fake = Faker()
+products = [
+  {
+    "id": 1,
+    "name": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    "price": 109.95,
+    "category_id": 4,
+    "image_url": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+  },
+  {
+    "id": 2,
+    "name": "Mens Casual Premium Slim Fit T-Shirts",
+    "price": 22.3,
+    "category_id": 4,
+    "image_url": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"
+  },
+  {
+    "id": 3,
+    "name": "Mens Cotton Jacket",
+    "price": 55.99,
+    "category_id": 4,
+    "image_url": "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg"
+  },
+  {
+    "id": 4,
+    "name": "Mens Casual Slim Fit",
+    "price": 15.99,
+    "category_id": 4,
+    "image_url": "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg"
+  },
+  {
+    "id": 5,
+    "name": "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
+    "price": 695,
+    "category_id": 3,
+    "image_url": "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg"
+  },
+  {
+    "id": 6,
+    "name": "Solid Gold Petite Micropave",
+    "price": 168,
+    "category_id": 3,
+    "image_url": "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg"
+  },
+  {
+    "id": 7,
+    "name": "White Gold Plated Princess",
+    "price": 9.99,
+    "category_id": 3,
+    "image_url": "https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg"
+  },
+  {
+    "id": 8,
+    "name": "Pierced Owl Rose Gold Plated Stainless Steel Double",
+    "price": 10.99,
+    "category_id": 3,
+    "image_url": "https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_.jpg"
+  },
+  {
+    "id": 9,
+    "name": "WD 2TB Elements Portable External Hard Drive - USB 3.0",
+    "price": 64,
+    "category_id": 2,
+    "image_url": "https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg"
+  },
+  {
+    "id": 10,
+    "name": "SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s",
+    "price": 109,
+    "category_id": 2,
+    "image_url": "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg"
+  },
+  {
+    "id": 11,
+    "name": "Silicon Power 256GB SSD 3D NAND A55 SLC Cache Performance Boost SATA III 2.5",
+    "price": 109,
+    "category_id": 2,
+    "image_url": "https://fakestoreapi.com/img/71kWymZ+c+L._AC_SX679_.jpg"
+  },
+  {
+    "id": 12,
+    "name": "WD 4TB Gaming Drive Works with Playstation 4 Portable External Hard Drive",
+    "price": 114,
+    "category_id": 2,
+    "image_url": "https://fakestoreapi.com/img/61mtL65D4cL._AC_SX679_.jpg"
+  },
+  {
+    "id": 13,
+    "name": "Acer SB220Q bi 21.5 inches Full HD (1920 x 1080) IPS Ultra-Thin",
+    "price": 599,
+    "category_id": 2,
+    "image_url": "https://fakestoreapi.com/img/81QpkIctqPL._AC_SX679_.jpg"
+  },
+  {
+    "id": 14,
+    "name": "Samsung 49-Inch CHG90 144Hz Curved Gaming Monitor (LC49HG90DMNXZA) – Super Ultrawide Screen QLED",
+    "price": 999.99,
+    "category_id": 2,
+    "image_url": "https://fakestoreapi.com/img/81Zt42ioCgL._AC_SX679_.jpg"
+  },
+  {
+    "id": 15,
+    "name": "BIYLACLESEN Women's 3-in-1 Snowboard Jacket Winter Coats",
+    "price": 56.99,
+    "category_id": 1,
+    "image_url": "https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg"
+  },
+  {
+    "id": 16,
+    "name": "Lock and Love Women's Removable Hooded Faux Leather Moto Biker Jacket",
+    "price": 29.95,
+    "category_id": 1,
+    "image_url": "https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_.jpg"
+  },
+  {
+    "id": 17,
+    "name": "Rain Jacket Women Windbreaker Striped Climbing Raincoats",
+    "price": 39.99,
+    "category_id": 1,
+    "image_url": "https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg"
+  },
+  {
+    "id": 18,
+    "name": "MBJ Women's Solid Short Sleeve Boat Neck V",
+    "price": 9.85,
+    "category_id": 1,
+    "image_url": "https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_.jpg"
+  },
+  {
+    "id": 19,
+    "name": "Opna Women's Short Sleeve Moisture",
+    "price": 7.95,
+    "category_id": 1,
+    "image_url": "https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg"
+  },
+  {
+    "id": 20,
+    "name": "DANVOUY Womens T Shirt Casual Cotton Short",
+    "price": 12.99,
+    "category_id": 1,
+    "image_url": "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg"
+  }
+]
 
 
-# Create an application context
-app.app_context().push()
+categories = [
+  {
+    "id": 1,
+    "name": "women's clothing"
+  },
+  {
+    "id": 2,
+    "name": "electronics"
+  },
+  {
+    "id": 3,
+    "name": "jewelery"
+  },
+  {
+    "id": 4,
+    "name": "men's clothing"
+  }
+];
 
-# products = [{
-#   "id": 1,
-#   "image_url": "http://dummyimage.com/209x100.png/dddddd/000000",
-#   "name": "Classic Television",
-#   "price": 23,
-#   "category_id": 1
-# }, {
-#   "id": 2,
-#   "image_url": "http://dummyimage.com/179x100.png/dddddd/000000",
-#   "name": "Sony Camera",
-#   "price": 78,
-#   "category_id": 2
-# }, {
-#   "id": 3,
-#   "image_url": "http://dummyimage.com/240x100.png/5fa2dd/ffffff",
-#   "name": "Blender",
-#   "price": 61,
-#   "category_id": 3
-# }, {
-#   "id": 4,
-#   "image_url": "http://dummyimage.com/201x100.png/ff4444/ffffff",
-#   "name": "Iphone 15",
-#   "price": 76,
-#   "category_id": 2
-# }, {
-#   "id": 5,
-#   "image_url": "http://dummyimage.com/106x100.png/cc0000/ffffff",
-#   "name": "Vintage Television",
-#   "price": 8,
-#   "category_id": 1
-# }, {
-#   "id": 6,
-#   "image_url": "http://dummyimage.com/215x100.png/5fa2dd/ffffff",
-#   "name": "Hand mixer",
-#   "price": 78,
-#   "category_id": 3
-# }, {
-#   "id": 7,
-#   "image_url": "http://dummyimage.com/173x100.png/ff4444/ffffff",
-#   "name": "Canon Lens",
-#   "price": 54,
-#   "category_id": 1
-# }, {
-#   "id": 8,
-#   "image_url": "http://dummyimage.com/111x100.png/ff4444/ffffff",
-#   "name": "Purple Phone",
-#   "price": 26,
-#   "category_id": 2
-# }, {
-#   "id": 9,
-#   "image_url": "http://dummyimage.com/120x100.png/ff4444/ffffff",
-#   "name": "Cooking gas",
-#   "price": 4,
-#   "category_id": 3
-# }, {
-#   "id": 10,
-#   "image_url": "http://dummyimage.com/161x100.png/cc0000/ffffff",
-#   "name": "Microwave",
-#   "price": 44,
-#   "category_id": 3
-# }]
 
-# categories = [{
-#   "id": 1,
-#   "name": "Entertainment"
-# }, {
-#   "id": 2,
-#   "name": "Communication"
-# }, {
-#   "id": 3,
-#   "name": "Kitchen Appliances"
-# }]
 
-# with app.app_context():
-#     db.session.add_all([Product(**rp) for  rp in products])
-#     db.session.commit()
-
-#     db.session.add_all([Category(**rp) for  rp in categories])
-#     db.session.commit()
-    
-electronics_words = ['laptop', 'smartphone', 'camera', 'headphones', 'tablet', 'smartwatch', 'router', 'keyboard', 'mouse', 'printer']
-category_ids=['1', '3', '2']
-
-def generate_fake_data():
-    # Generate fake data
-    # users = [User(name=fake.name(), email=fake.email()) for _ in range(10)]
-    products = [
-        Product(
-            name=fake.random_element(elements=electronics_words),
-            price=fake.random_number(2),
-            category_id=fake.random_element(elements=category_ids),
-            image_url=fake.image_url()
-        ) for _ in range(10)
-    ]
-    # carts = [Cart(user=user) for user in users]
-    # addresses = [
-    #     Address(user=user, street=fake.street_address(), city=fake.city()) for user in users
-    # ]
-
-    # Add data to the database session
-    db.session.add_all(products)
-
-    # Commit the changes to the database
+with app.app_context():
+    db.session.add_all([Product(**rp) for  rp in products])
     db.session.commit()
 
-if __name__ == '__main__':
-    generate_fake_data()
+    db.session.add_all([Category(**rp) for  rp in categories])
+    db.session.commit()
+    
 
